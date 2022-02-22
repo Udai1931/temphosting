3a5b6acb3a4b2cef233d733a5b51cd5206e79c5c
Udai Gupta <udaigupta19311@gmail.com>
2022-02-22 19:51:48 +0530

    a

--- pages/forgotpassword/index.js
+++ pages/forgotpassword/index.js
@@ -9,7 +9,7 @@ import bg1 from '../../assets/bg1.jpg'
 import bg2 from '../../assets/bg2.jpg'
 import bg3 from '../../assets/bg3.jpg'
 import { Carousel } from 'react-responsive-carousel';
-import { AuthContext } from '../../../context/auth';
+import { AuthContext } from '../../context/auth';
 import { useRouter } from 'next/router';
 function Index() {
 
